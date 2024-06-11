9a682f2364568fb9698003f17d06c13ff1454547
Rustam Abdusamatov <ruskor2020mos@gmail.com>
2024-06-11 22:43:18 +0300

    done: 1. Separate auth user and crud operations ------------------------------------------ The Application is working ----------------------------------------- todo: 1. Add handling for login and registry errors; 2. Add getUsersList method; 3. Add randomise creating user and book

--- src/main/java/ru/abdusamatov/librarywithsecurity/controllers/UserController.java
+++ src/main/java/ru/abdusamatov/librarywithsecurity/controllers/UserController.java
@@ -2,35 +2,9 @@ package ru.abdusamatov.librarywithsecurity.controllers;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import ru.abdusamatov.librarywithsecurity.models.User;
-import ru.abdusamatov.librarywithsecurity.services.UserService;
 
 @Controller
 @RequiredArgsConstructor
 public class UserController {
-    private final UserService userService;
 
-    @GetMapping("/registration")
-    public String registration(@ModelAttribute("user") User user){
-        return "users/registration";
-    }
-
-    @PostMapping("/registration")
-    public String createUser(@ModelAttribute ("user")  User user, Model model){
-        if (!userService.createUser(user)){
-            model.addAttribute("errorEmail",
-                    "User with email: " + user.getEmail() + " already exists!");
-            return "users/registration";
-        }
-        return "login";
-    }
-
-    @GetMapping("/login")
-    public String login(){
-        return "login";
-    }
 }
