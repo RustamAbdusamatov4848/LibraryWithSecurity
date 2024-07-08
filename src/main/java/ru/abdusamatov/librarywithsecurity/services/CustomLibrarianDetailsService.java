35f32e2d32c319679e63b16e83b25df0aa392df0
Rustam Abdusamatov <ruskor2020mos@gmail.com>
2024-07-08 13:24:43 +0300

    done: Add test for CustomLibrarianDetailsServiceTest ------------------------------------------ The Application is working ----------------------------------------- todo: 1. Add search user 1. Add test for Librarian.class

--- src/main/java/ru/abdusamatov/librarywithsecurity/services/CustomLibrarianDetailsService.java
+++ src/main/java/ru/abdusamatov/librarywithsecurity/services/CustomLibrarianDetailsService.java
@@ -5,14 +5,20 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import ru.abdusamatov.librarywithsecurity.models.Librarian;
 import ru.abdusamatov.librarywithsecurity.repositories.LibrarianRepository;
 
 @Service
 @RequiredArgsConstructor
 public class CustomLibrarianDetailsService implements UserDetailsService {
     private final LibrarianRepository repository;
+
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return repository.findByEmail(email);
+        Librarian librarian = repository.findByEmail(email);
+        if (librarian == null) {
+            throw new UsernameNotFoundException("Librarian not found with email: " + email);
+        }
+        return librarian;
     }
 }
