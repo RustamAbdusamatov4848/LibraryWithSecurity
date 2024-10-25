9d8e1971f1a4d21ccb1e15e27d95cfa85ebf82a3
Rustam Abdusamatov <ruskor2020mos@gmail.com>
2024-10-25 17:35:43 +0300

    Refactor librarian service

--- src/main/java/ru/abdusamatov/librarywithsecurity/services/LibrarianService.java
+++ src/main/java/ru/abdusamatov/librarywithsecurity/services/LibrarianService.java
@@ -5,8 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ru.abdusamatov.librarywithsecurity.dto.LibrarianDto;
 import ru.abdusamatov.librarywithsecurity.models.Librarian;
 import ru.abdusamatov.librarywithsecurity.repositories.LibrarianRepository;
+import ru.abdusamatov.librarywithsecurity.util.mappers.LibrarianMapper;
 
 @Service
 @Slf4j
@@ -14,14 +16,13 @@ import ru.abdusamatov.librarywithsecurity.repositories.LibrarianRepository;
 public class LibrarianService {
     private final LibrarianRepository librarianRepository;
     private final PasswordEncoder passwordEncoder;
+    private final LibrarianMapper librarianMapper;
 
     @Transactional
-    public boolean createLibrarian(Librarian librarian) {
-        String userEmail = librarian.getEmail();
-        if (librarianRepository.findByEmail(userEmail) != null) return false;
-        librarian.setPassword(passwordEncoder.encode(librarian.getPassword()));
-        log.info("Saving new Librarian with email: {}", userEmail);
-        librarianRepository.save(librarian);
-        return true;
+    public void createLibrarian(LibrarianDto librarianDto) {
+        Librarian librarian = librarianMapper.librarianDtoToLibrarian(librarianDto);
+        librarian.setPassword(passwordEncoder.encode(librarianDto.getPassword()));
+        librarian = librarianRepository.save(librarian);
+        log.info("Saving new Librarian with ID: {}", librarian.getId());
     }
 }
