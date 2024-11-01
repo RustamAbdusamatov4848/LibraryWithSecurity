230cdabad18a2c53ee4022b75e614ae843b1130c
Rustam Abdusamatov <ruskor2020mos@gmail.com>
2024-11-01 15:12:33 +0300

    Refactor exception response type

--- src/main/java/ru/abdusamatov/librarywithsecurity/controllers/handlers/GlobalExceptionHandler.java
+++ src/main/java/ru/abdusamatov/librarywithsecurity/controllers/handlers/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package ru.abdusamatov.librarywithsecurity.controllers.handlers;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -8,23 +10,15 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import ru.abdusamatov.librarywithsecurity.errors.ErrorResponse;
 import ru.abdusamatov.librarywithsecurity.exceptions.ExistEmailException;
 import ru.abdusamatov.librarywithsecurity.exceptions.ResourceNotFoundException;
-import ru.abdusamatov.librarywithsecurity.util.Response;
 
 import java.util.HashMap;
 import java.util.Map;
-
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.NOT_FOUND;
-import static org.springframework.http.HttpStatus.UNAUTHORIZED;
-import static ru.abdusamatov.librarywithsecurity.errors.ErrorResponse.buildError;
-import static ru.abdusamatov.librarywithsecurity.util.Response.buildResponse;
-import static ru.abdusamatov.librarywithsecurity.util.Result.error;
 
 @ControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Response<ErrorResponse> handleValidationException(MethodArgumentNotValidException ex) {
+    public ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException ex) {
         log.error("Validation failed: {}", ex.getMessage(), ex);
 
         Map<String, String> errors = new HashMap<>();
@@ -33,36 +27,41 @@ public class GlobalExceptionHandler {
                 .forEach(error -> errors.put(error.getField(), error.getDefaultMessage())
                 );
 
-        return buildResponse(error(BAD_REQUEST), buildError("Validation field failed", errors));
+        ErrorResponse errorResponse = new ErrorResponse("Validation field failed", errors);
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public Response<ErrorResponse> handleEntityNotFoundException(ResourceNotFoundException ex) {
+    public ResponseEntity<ErrorResponse> handleEntityNotFoundException(ResourceNotFoundException ex) {
         log.error("Failed entity search: {}", ex.getMessage(), ex);
 
         String message = ex.getMessage();
         Map<String, String> errors = Map.of("cause", message);
 
-        return buildResponse(error(NOT_FOUND), buildError("Failed entity search", errors));
+        ErrorResponse errorResponse = new ErrorResponse("Failed entity search", errors);
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(AuthenticationException.class)
-    public Response<ErrorResponse> handleAuthenticationException(AuthenticationException ex) {
+    public ResponseEntity<ErrorResponse> handleAuthenticationException(AuthenticationException ex) {
         log.error("Failed authorization: {}", ex.getMessage(), ex);
 
         String message = ex.getMessage();
         Map<String, String> errors = Map.of("cause", message);
 
-        return buildResponse(error(UNAUTHORIZED), buildError("Failed authorization", errors));
+        ErrorResponse errorResponse = new ErrorResponse("Failed authorization", errors);
+        return new ResponseEntity<>(errorResponse, HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(ExistEmailException.class)
-    public Response<ErrorResponse> handleExistEmailException(ExistEmailException ex) {
+    public ResponseEntity<ErrorResponse> handleExistEmailException(ExistEmailException ex) {
         log.error("Failed email validation: {}", ex.getMessage(), ex);
 
         String message = ex.getMessage();
         Map<String, String> errors = Map.of("cause", message);
 
-        return buildResponse(error(BAD_REQUEST), buildError("Failed email validation, already exist", errors));
+        ErrorResponse errorResponse = new ErrorResponse("Failed email validation, already exist", errors);
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
+
 }
