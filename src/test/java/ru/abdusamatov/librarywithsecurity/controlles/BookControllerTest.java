c147c635a86d4c342ecec86b07098adf497d9ebf
Rustam Abdusamatov <ruskor2020mos@gmail.com>
2024-11-06 15:51:00 +0300

    Add tests for book controller

--- src/test/java/ru/abdusamatov/librarywithsecurity/controlles/BookControllerTest.java
+++ src/test/java/ru/abdusamatov/librarywithsecurity/controlles/BookControllerTest.java
@@ -6,23 +6,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import ru.abdusamatov.librarywithsecurity.dto.BookDto;
+import ru.abdusamatov.librarywithsecurity.dto.UserDto;
 import ru.abdusamatov.librarywithsecurity.repositories.BookRepository;
 import ru.abdusamatov.librarywithsecurity.repositories.UserRepository;
 import ru.abdusamatov.librarywithsecurity.services.BookService;
 import ru.abdusamatov.librarywithsecurity.services.UserService;
 import ru.abdusamatov.librarywithsecurity.support.TestDataProvider;
-import ru.abdusamatov.librarywithsecurity.util.Response;
-import ru.abdusamatov.librarywithsecurity.util.ResponseStatus;
 
 import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @Testcontainers
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -57,34 +54,198 @@ public class BookControllerTest {
     @Test
     void shouldGetBookList() {
         int bookListSize = 10;
-        List<BookDto> bookDtoList = TestDataProvider.createSampleListBookDto(bookListSize);
+        List<BookDto> bookDtoList = TestDataProvider.createListBookDto(bookListSize);
         bookDtoList.forEach(bookDto -> bookService.createBook(bookDto));
 
-        webTestClient.get().uri("/books?page=0&size=10").exchange().expectStatus().isOk().expectBody(Response.class).value(response -> {
-            assertThat(response.getResult().getStatus()).isEqualTo(ResponseStatus.SUCCESS);
-            assertThat(response.getResult().getHttpStatusCode()).isEqualTo(HttpStatus.OK);
-            assertThat(response.getResult().getDescription()).isEqualTo("List of books");
-            assertThat(response.getData()).isNotNull();
-        });
+        webTestClient.get().uri("/books?page=0&size=10")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.result.httpStatusCode").isEqualTo("OK")
+                .jsonPath("$.result.status").isEqualTo("SUCCESS")
+                .jsonPath("$.result.description").isEqualTo("List of books");
     }
 
     @Test
     void shouldReturnBook_whenExistingBookIdProvided() {
         long id = 1L;
-        bookService.createBook(TestDataProvider.createSampleBookDto());
+        bookService.createBook(TestDataProvider.createBookDto());
 
-        webTestClient.get().uri("/books/" + id).exchange().expectStatus().isOk().expectBody(Response.class).value(response -> {
-            assertThat(response.getResult().getStatus()).isEqualTo(ResponseStatus.SUCCESS);
-            assertThat(response.getResult().getHttpStatusCode()).isEqualTo(HttpStatus.OK);
-            assertThat(response.getResult().getDescription()).isEqualTo("Book successfully found");
-            assertThat(response.getData()).isNotNull();
-        });
+        webTestClient.get().uri("/books/" + id)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.data.id").isEqualTo(id)
+                .jsonPath("$.result.httpStatusCode").isEqualTo("OK")
+                .jsonPath("$.result.status").isEqualTo("SUCCESS")
+                .jsonPath("$.result.description").isEqualTo("Book successfully found");
     }
 
     @Test
     void shouldReturnNotFound_whenNonExistingBookIdProvided() {
         long id = 1L;
 
-        webTestClient.get().uri("/books/" + id).exchange().expectStatus().isNotFound();
+        webTestClient.get().uri("/books/" + id)
+                .exchange()
+                .expectStatus().isNotFound()
+                .expectBody()
+                .jsonPath("$.message").isEqualTo("Failed entity search")
+                .jsonPath("$.errors.cause").isEqualTo("Book with ID: " + id + ", not found");
     }
+
+    @Test
+    void shouldCreateBook_whenValidBookDtoProvided() {
+        BookDto validBookDto = TestDataProvider.createBookDto();
+
+        webTestClient.post().uri("/books")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(validBookDto)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.result.httpStatusCode").isEqualTo("CREATED")
+                .jsonPath("$.result.status").isEqualTo("SUCCESS")
+                .jsonPath("$.result.description").isEqualTo("Book successfully created")
+                .jsonPath("$.data.title").isEqualTo(validBookDto.getTitle())
+                .jsonPath("$.data.authorName").isEqualTo(validBookDto.getAuthorName())
+                .jsonPath("$.data.authorSurname").isEqualTo(validBookDto.getAuthorSurname())
+                .jsonPath("$.data.yearOfPublication").isEqualTo(validBookDto.getYearOfPublication());
+    }
+
+    @Test
+    void shouldReturnBadRequest_whenInvalidBookDataProvided() {
+        BookDto invalidBookDto = TestDataProvider.createInvalidBookDto();
+
+        webTestClient.post().uri("/books")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(invalidBookDto)
+                .exchange()
+                .expectStatus().isBadRequest()
+                .expectBody()
+                .jsonPath("$.message").isEqualTo("Validation field failed")
+                .jsonPath("$.errors.authorName").isEqualTo("Author name must be between 2 and 30 characters long")
+                .jsonPath("$.errors.authorSurname").isEqualTo("Author surname must be between 2 and 30 characters long")
+                .jsonPath("$.errors.yearOfPublication").isEqualTo("Year must be greater than 1500");
+    }
+
+    @Test
+    void shouldUpdateBook_whenValidBookDtoProvided() {
+        BookDto bookToBeUpdated = bookService.createBook(TestDataProvider.createBookDto());
+        BookDto updateBookDto = TestDataProvider.updatedBookDto(bookToBeUpdated);
+
+        webTestClient.put().uri("/books")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(updateBookDto)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.result.httpStatusCode").isEqualTo("OK")
+                .jsonPath("$.result.status").isEqualTo("SUCCESS")
+                .jsonPath("$.result.description").isEqualTo("Book successfully updated")
+                .jsonPath("$.data.id").isEqualTo(updateBookDto.getId())
+                .jsonPath("$.data.title").isEqualTo(updateBookDto.getTitle())
+                .jsonPath("$.data.authorName").isEqualTo(updateBookDto.getAuthorName())
+                .jsonPath("$.data.authorSurname").isEqualTo(updateBookDto.getAuthorSurname())
+                .jsonPath("$.data.yearOfPublication").isEqualTo(updateBookDto.getYearOfPublication())
+                .jsonPath("$.data.takenAt").isEqualTo(updateBookDto.getTakenAt())
+                .jsonPath("$.data.userId").isEqualTo(updateBookDto.getUserId());
+    }
+
+    @Test
+    void shouldReturnNotFound_whenBookToUpdateDoesNotExist() {
+        long notExistingId = 10000L;
+
+        BookDto bookToBeUpdated = bookService.createBook(TestDataProvider.createBookDto());
+        BookDto updateBookDto = TestDataProvider.updatedBookDto(bookToBeUpdated);
+        updateBookDto.setId(notExistingId);
+
+        webTestClient.put().uri("/books")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(updateBookDto)
+                .exchange()
+                .expectStatus().isNotFound()
+                .expectBody()
+                .jsonPath("$.message").isEqualTo("Failed entity search")
+                .jsonPath("$.errors.cause").isEqualTo("Book with ID: " + notExistingId + ", not found");
+    }
+
+    @Test
+    void shouldReturnBadRequest_whenUpdateInvalidBookDataProvided() {
+        BookDto bookToBeUpdated = bookService.createBook(TestDataProvider.createBookDto());
+        BookDto invalidBookDto = TestDataProvider.updatedBookDtoWithInvalidField(bookToBeUpdated);
+
+        webTestClient.put().uri("/books")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(invalidBookDto)
+                .exchange()
+                .expectStatus().isBadRequest()
+                .expectBody()
+                .jsonPath("$.message").isEqualTo("Validation field failed")
+                .jsonPath("$.errors.authorName").isEqualTo("Author name must be between 2 and 30 characters long")
+                .jsonPath("$.errors.authorSurname").isEqualTo("Author surname must be between 2 and 30 characters long")
+                .jsonPath("$.errors.yearOfPublication").isEqualTo("Year must be greater than 1500");
+    }
+
+    @Test
+    void shouldReturnNoContent_whenBookDeletedSuccessfully() {
+        BookDto bookDtoToBeDeleted = bookService.createBook(TestDataProvider.createBookDto());
+        long id = bookDtoToBeDeleted.getId();
+
+        webTestClient.delete().uri("/books/" + id)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.result.httpStatusCode").isEqualTo("NO_CONTENT")
+                .jsonPath("$.result.status").isEqualTo("SUCCESS")
+                .jsonPath("$.result.description").isEqualTo("Successfully deleted");
+
+    }
+
+    @Test
+    void shouldReturnNotFound_whenBookToDeleteDoesNotExist() {
+        long notExistingId = 10000L;
+
+        webTestClient.delete().uri("/books/" + notExistingId)
+                .exchange()
+                .expectStatus().isNotFound()
+                .expectBody()
+                .jsonPath("$.message").isEqualTo("Failed entity search")
+                .jsonPath("$.errors.cause").isEqualTo("Book with ID: " + notExistingId + ", not found");
+
+    }
+
+    @Test
+    void shouldAssignBook_whenValidBookIdAndUserProvided() {
+        long bookId = bookService.createBook(TestDataProvider.createBookDto()).getId();
+        UserDto userDtoToBeAssigned = userService.createUser(TestDataProvider.createUserDto());
+
+        webTestClient.patch().uri("/books/" + bookId + "/assign")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(userDtoToBeAssigned)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.result.httpStatusCode").isEqualTo("NO_CONTENT")
+                .jsonPath("$.result.status").isEqualTo("SUCCESS")
+                .jsonPath("$.result.description").isEqualTo("Successfully assigned");
+    }
+
+    @Test
+    void shouldReturnNotFound_whenBookToAssignDoesNotExist() {
+
+    }
+
+    @Test
+    void shouldReleaseBook_whenValidBookIdProvided() {
+
+    }
+
+    void shouldReturnNotFound_whenBookToReleaseDoesNotExist() {
+
+    }
+
+    void shouldReturnBooks_whenValidQueryProvided() {
+
+    }
+
 }
--- src/test/java/ru/abdusamatov/librarywithsecurity/support/TestDataProvider.java
+++ src/test/java/ru/abdusamatov/librarywithsecurity/support/TestDataProvider.java
@@ -8,9 +8,65 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-public class TestDataProvider {
 
-    public static UserDto createSampleUserDto() {
+public class TestDataProvider {
+    public static final int MAX_NAME_LENGTH = 30;
+    public static final String LONG_NAME = "a".repeat(MAX_NAME_LENGTH + 1);
+    public static final int INVALID_YEAR_OF_PUBLICATION = 1499;
+    public static final String INVALID_EMAIL = "invalid-email";
+    public static final LocalDate INVALID_DATA_OF_BIRTH = LocalDate.now().minusDays(1);
+
+    public static BookDto createBookDto() {
+        return BookDto.builder()
+                .title("Book Title" + getLimitUUID())
+                .authorName("AuthorName")
+                .authorSurname("AuthorSurname")
+                .yearOfPublication(2020)
+                .build();
+    }
+
+    public static BookDto createInvalidBookDto() {
+        return BookDto.builder()
+                .title("Book Title" + getLimitUUID())
+                .authorName(LONG_NAME)
+                .authorSurname(LONG_NAME)
+                .yearOfPublication(INVALID_YEAR_OF_PUBLICATION)
+                .build();
+    }
+
+    public static BookDto updatedBookDto(BookDto bookToBeUpdated) {
+        BookDto updatedBookDto = new BookDto();
+        updatedBookDto.setId(bookToBeUpdated.getId());
+        updatedBookDto.setTitle("Title updated ");
+        updatedBookDto.setAuthorName("Author name updated");
+        updatedBookDto.setAuthorSurname("Author surname updated");
+        updatedBookDto.setYearOfPublication(bookToBeUpdated.getYearOfPublication());
+        updatedBookDto.setUserId(bookToBeUpdated.getUserId());
+        updatedBookDto.setTakenAt(bookToBeUpdated.getTakenAt());
+
+        return updatedBookDto;
+    }
+
+    public static BookDto updatedBookDtoWithInvalidField(BookDto bookToBeUpdated) {
+        BookDto bookDto = updatedBookDto(bookToBeUpdated);
+        bookDto.setAuthorName(LONG_NAME);
+        bookDto.setAuthorSurname(LONG_NAME);
+        bookDto.setYearOfPublication(INVALID_YEAR_OF_PUBLICATION);
+
+        return bookDto;
+    }
+
+    public static List<BookDto> createListBookDto(int size) {
+        List<BookDto> list = new ArrayList<>(size);
+
+        for (int i = 0; i < size; i++) {
+            list.add(createBookDto());
+        }
+
+        return list;
+    }
+
+    public static UserDto createUserDto() {
         return UserDto.builder()
                 .fullName("Test User" + getLimitUUID(10))
                 .email("testuser" + getLimitUUID(10) + "@example.com")
@@ -18,33 +74,37 @@ public class TestDataProvider {
                 .build();
     }
 
-    public static BookDto createSampleBookDto() {
-        return BookDto.builder()
-                .title("Book Title" + getLimitUUID(15))
-                .authorName("AuthorName")
-                .authorSurname("AuthorSurname")
-                .yearOfPublication(2020)
+    public static UserDto createInvalidUserDto() {
+        return UserDto.builder()
+                .fullName(LONG_NAME)
+                .email(INVALID_EMAIL)
+                .dateOfBirth(INVALID_DATA_OF_BIRTH)
                 .build();
     }
 
-    public static BookDto createSampleBookDtoWithOwner(Long userId) {
-        BookDto book = createSampleBookDto();
-        book.setUserId(userId);
-        return book;
-    }
-
-    public static List<BookDto> createSampleListBookDto(int size) {
-        List<BookDto> list = new ArrayList<>(size);
+    public static List<UserDto> createListUserDto(int size) {
+        List<UserDto> list = new ArrayList<>(size);
 
         for (int i = 0; i < size; i++) {
-            list.add(createSampleBookDto());
+            list.add(createUserDto());
         }
 
         return list;
     }
 
+
+    public static BookDto createBookDtoWithOwner(Long userId) {
+        BookDto book = createBookDto();
+        book.setUserId(userId);
+        return book;
+    }
+
     private static String getLimitUUID(int limit) {
         UUID uuid = UUID.randomUUID();
         return uuid.toString().replace("-", "").substring(0, limit + 1);
+    }
+
+    private static UUID getLimitUUID() {
+        return UUID.randomUUID();
     }
 }
