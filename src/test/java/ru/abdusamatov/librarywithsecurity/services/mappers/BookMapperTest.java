5e00567a383cb66908c0ceab8b6e5e1b5c888ca5
Rustam Abdusamatov <ruskor2020mos@gmail.com>
2024-11-08 16:09:31 +0300

    Refactor bookmapper

--- src/test/java/ru/abdusamatov/librarywithsecurity/services/mappers/BookMapperTest.java
+++ src/test/java/ru/abdusamatov/librarywithsecurity/services/mappers/BookMapperTest.java
@@ -1,8 +1,6 @@
 package ru.abdusamatov.librarywithsecurity.services.mappers;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mapstruct.factory.Mappers;
 import ru.abdusamatov.librarywithsecurity.dto.BookDto;
 import ru.abdusamatov.librarywithsecurity.models.Book;
 import ru.abdusamatov.librarywithsecurity.support.TestDataProvider;
@@ -11,39 +9,31 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class BookMapperTest {
 
-    private BookMapper mapper;
-
-    @BeforeEach
-    void setUp() {
-        mapper = Mappers.getMapper(BookMapper.class);
-    }
+    private final BookMapper mapper = new BookMapperImpl();
 
     @Test
-    void shouldMapBookToBookDto() {
+    void shouldMapBookToDto() {
         Book book = TestDataProvider.createBook();
 
         BookDto bookDto = mapper.bookToBookDto(book);
 
         assertThat(bookDto).isNotNull();
-        assertEquals(bookDto, book);
+        assertBookAndBookDtoEqual(bookDto, book);
     }
 
     @Test
-    void shouldMapBookDtoToBook() {
+    void shouldMapDtoToBook() {
         BookDto bookDto = TestDataProvider.createBookDto();
-        bookDto.setId(1L);
 
         Book book = mapper.bookDtoToBook(bookDto);
 
         assertThat(book).isNotNull();
-        assertEquals(bookDto, book);
+        assertBookAndBookDtoEqual(bookDto, book);
     }
 
     @Test
-    void shouldMapBookDtoToBook_whenBookDtoNull() {
-        BookDto bookDto = null;
-
-        Book book = mapper.bookDtoToBook(bookDto);
+    void shouldMapDtoToBook_whenDtoIsNull() {
+        Book book = mapper.bookDtoToBook(null);
 
         assertThat(book).isNull();
     }
@@ -57,49 +47,33 @@ public class BookMapperTest {
 
         Book updatedBook = mapper.updateBookFromDto(newBookDto, bookToBeUpdated);
 
-        assertThat(updatedBook).isNotNull();
-        assertEquals(newBookDto, updatedBook);
+        assertBookAndBookDtoEqual(newBookDto, updatedBook);
     }
 
     @Test
-    void shouldUpdateBookFromDto_whenBookDtoIsNull() {
+    void shouldUpdateBookFromDto_whenDtoIsNull() {
         Book bookToBeUpdated = TestDataProvider.createBook();
-        BookDto newBookDto = null;
 
-        Book updatedBook = mapper.updateBookFromDto(newBookDto,bookToBeUpdated);
+        Book updatedBook = mapper.updateBookFromDto(null, bookToBeUpdated);
 
-        assertThat(updatedBook).isEqualTo(bookToBeUpdated);
+        assertThat(updatedBook)
+                .usingRecursiveComparison()
+                .isEqualTo(bookToBeUpdated);
     }
 
     @Test
-    void shouldReturnNull_whenBookIsNullInBookToBookDto() {
+    void shouldReturnNull_whenBookIsNullInBookToDto() {
         BookDto bookDto = mapper.bookToBookDto(null);
+
         assertThat(bookDto).isNull();
     }
 
-    @Test
-    void shouldReturnNull_whenBookDtoIsNullInBookDtoToBook() {
-        Book book = mapper.bookDtoToBook(null);
-        assertThat(book).isNull();
-    }
-
-    private static void assertEquals(BookDto bookDto, Book book) {
-        assertThat(bookDto).extracting(
-                BookDto::getId,
-                BookDto::getTitle,
-                BookDto::getAuthorName,
-                BookDto::getAuthorSurname,
-                BookDto::getYearOfPublication,
-                BookDto::getTakenAt,
-                BookDto::getUserId
-        ).containsExactly(
-                book.getId(),
-                book.getTitle(),
-                book.getAuthorName(),
-                book.getAuthorSurname(),
-                book.getYearOfPublication(),
-                book.getTakenAt(),
-                book.getOwner().getId()
-        );
+    private static void assertBookAndBookDtoEqual(BookDto bookDto, Book book) {
+        assertThat(book)
+                .withFailMessage("Books are not equal")
+                .usingRecursiveComparison()
+                .ignoringFields("owner")
+                .isEqualTo(bookDto);
+        assertThat(book.getOwner().getId()).isEqualTo(bookDto.getUserId());
     }
 }
