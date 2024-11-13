3d85e63b0fc22090c277013d7c8615aaaa65968a
Rustam Abdusamatov <ruskor2020mos@gmail.com>
2024-11-13 07:32:59 +0300

    Refactor librarian test mapper

--- src/test/java/ru/abdusamatov/librarywithsecurity/service/mapper/LibrarianMapperTest.java
+++ src/test/java/ru/abdusamatov/librarywithsecurity/service/mapper/LibrarianMapperTest.java
@@ -1,9 +1,14 @@
 package ru.abdusamatov.librarywithsecurity.service.mapper;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import ru.abdusamatov.librarywithsecurity.dto.LibrarianDto;
 import ru.abdusamatov.librarywithsecurity.model.Librarian;
 import ru.abdusamatov.librarywithsecurity.support.TestDataProvider;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -11,49 +16,69 @@ public class LibrarianMapperTest {
 
     private final LibrarianMapper mapper = new LibrarianMapperImpl();
 
-    @Test
-    void shouldMapLibrarianToLibrarianDto() {
-        final var librarian = TestDataProvider
-                .createLibrarian()
-                .build();
+    @ParameterizedTest
+    @MethodSource("shouldMapLibrarianToDto")
+    void shouldMapLibrarianToDto(Librarian toBeMapped, LibrarianDto expected) {
+        final var actual = mapper.librarianToLibrarianDto(toBeMapped);
 
-        final var librarianDto = mapper.librarianToLibrarianDto(librarian);
-
-        assertThat(librarianDto)
-                .isNotNull();
-        assertLibrariansAreEqual(librarianDto, librarian);
+        assertThat(actual)
+                .usingRecursiveComparison()
+                .ignoringFields("password")
+                .isEqualTo(expected);
     }
 
-    @Test
-    void shouldMapLibrarianDtoToLibrarian() {
-        final var librarianDto = TestDataProvider
-                .createLibrarianDto()
-                .id(1L)
-                .build();
+    @ParameterizedTest
+    @MethodSource("shouldMapDtoToLibrarian")
+    void shouldMapDtoToLibrarian(LibrarianDto dtoToBeUpdated, Librarian expected) {
+        final var actual = mapper.librarianDtoToLibrarian(dtoToBeUpdated);
 
-        final var librarian = mapper.librarianDtoToLibrarian(librarianDto);
-
-        assertThat(librarian)
-                .isNotNull();
-        assertLibrariansAreEqual(librarianDto, librarian);
+        assertThat(actual)
+                .usingRecursiveComparison()
+                .ignoringFields("password")
+                .isEqualTo(expected);
     }
 
     @Test
     void shouldReturnNull_whenLibrarianIsNullInLibrarianToDto() {
         final var librarianDto = mapper.librarianToLibrarianDto(null);
+
         assertThat(librarianDto).isNull();
     }
 
     @Test
-    void shouldReturnNull_whenLibrarianDtoIsNullInDtoToLibrarian() {
+    void shouldReturnNull_whenDtoIsNullInDtoToLibrarian() {
         final var librarian = mapper.librarianDtoToLibrarian(null);
+
         assertThat(librarian).isNull();
     }
 
-    private static void assertLibrariansAreEqual(final LibrarianDto librarianDto, final Librarian librarian) {
-        assertThat(librarian)
-                .usingRecursiveComparison()
-                .ignoringFields("password")
-                .isEqualTo(librarianDto);
+    public static Stream<Arguments> shouldMapLibrarianToDto() {
+        final var librarian = TestDataProvider
+                .createLibrarian()
+                .build();
+
+        LibrarianDto expected = TestDataProvider
+                .createLibrarianDto()
+                .id(librarian.getId())
+                .fullName(librarian.getFullName())
+                .email(librarian.getEmail())
+                .build();
+
+        return Stream.of(Arguments.arguments(librarian, expected));
+    }
+
+    public static Stream<Arguments> shouldMapDtoToLibrarian() {
+        LibrarianDto dtoToBeMapped = TestDataProvider
+                .createLibrarianDto()
+                .build();
+
+        Librarian expected = TestDataProvider
+                .createLibrarian()
+                .id(dtoToBeMapped.getId())
+                .fullName(dtoToBeMapped.getFullName())
+                .email(dtoToBeMapped.getEmail())
+                .build();
+
+        return Stream.of(Arguments.arguments(dtoToBeMapped, expected));
     }
 }
