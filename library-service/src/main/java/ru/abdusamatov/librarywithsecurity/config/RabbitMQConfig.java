244349653490138cb84d622c898d8c1c82af32df
Rustam Abdusamatov <ruskor2020mos@gmail.com>
2025-01-08 10:08:01 +0300

    Add rabbit base config

--- library-service/src/main/java/ru/abdusamatov/librarywithsecurity/config/RabbitMQConfig.java
+++ library-service/src/main/java/ru/abdusamatov/librarywithsecurity/config/RabbitMQConfig.java
@@ -1,6 +1,9 @@
 package ru.abdusamatov.librarywithsecurity.config;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,8 +14,24 @@ public class RabbitMQConfig {
     @Value("${spring.rabbitmq.queues.notification-queue}")
     private String queueName;
 
+    @Value("${spring.rabbitmq.exchange}")
+    private String exchangeName;
+
     @Bean
     public Queue queue() {
         return new Queue(queueName, false);
+    }
+
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(exchangeName);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder
+                .bind(queue)
+                .to(exchange)
+                .with("library.notification.#");
     }
 }
