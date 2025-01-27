0b03f40d61e6b2ee515c22317f88af61738d30fa
r.abdusamatov <ruskor2020mos@gmail.com>
2025-01-27 12:17:02 +0300

    Add base entity for OutboxDomainEvent

---
+++ src/main/java/ru/abdusamatov/librarywithsecurity/model/BaseEntity.java
@@ -0,0 +1,27 @@
+package ru.abdusamatov.librarywithsecurity.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@MappedSuperclass
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+}
--- src/main/java/ru/abdusamatov/librarywithsecurity/model/OutboxDomainEvent.java
+++ src/main/java/ru/abdusamatov/librarywithsecurity/model/OutboxDomainEvent.java
@@ -2,9 +2,6 @@ package ru.abdusamatov.librarywithsecurity.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -19,12 +16,7 @@ import lombok.Setter;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class OutboxDomainEvent {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    private Long id;
+public class OutboxDomainEvent extends BaseEntity {
 
     @Column(name = "user_name")
     private String userName;
